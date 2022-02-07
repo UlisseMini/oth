3840da4069ea0e9e6ff753a832e28cb7a52aee36
Ulisse Mini <ulisse.mini@gmail.com>
2022-02-07 14:01:21 -0500

    Use css from katex and highlight in node_modules

--- main.js
+++ main.js
@@ -10,7 +10,7 @@ import remarkMath from "remark-math";
 import remarkWikiLink from "remark-wiki-link";
 import klaw from "klaw";
 import path from "path";
-import { mkdir } from "fs/promises";
+import fs from "fs-extra";
 import frontmatter from "front-matter";
 
 main();
@@ -26,19 +26,16 @@ async function main() {
       htmlVFile.extname = ".html";
       htmlVFile.stem = pageResolver(sourceVFile.stem);
 
-      await mkdir(htmlVFile.dirname, { recursive: true });
+      await fs.mkdir(htmlVFile.dirname, { recursive: true });
       await write(htmlVFile);
       console.log(`wrote ${htmlVFile.path}`);
     }
   }
-}
 
-const pageResolver = (name) => name.toLowerCase().replace(/ /g, "-");
-const depth = (dirname) =>
-  dirname
-    .split("/")
-    .reverse()
-    .findIndex((p) => p === "notes");
+  await vendor("katex/dist/katex.min.css");
+  await vendor("katex/dist/fonts");
+  await vendor("highlight.js/styles/default.css", "highlight.css");
+}
 
 async function compile(file) {
   const fm = frontmatter(file.value.toString());
@@ -64,10 +61,24 @@ async function compile(file) {
       // Could fetch @latest css, but I'm afraid of breaking changes (eg. class name changes)
       css: [
         root + "styles.css",
-        root + "highlight.min.css",
+        root + "highlight.css",
         root + "katex.min.css",
       ],
     })
     .use(rehypeStringify)
     .process(file);
+}
+
+const pageResolver = (name) => name.toLowerCase().replace(/ /g, "-");
+const depth = (dirname) =>
+  dirname
+    .split("/")
+    .reverse()
+    .findIndex((p) => p === "notes");
+
+async function vendor(p, out) {
+  const src = path.join("node_modules", p);
+  const dst = path.join("out", out || p.split("/").at(-1));
+  await fs.copy(src, dst);
+  console.log(`copied ${src} -> ${dst}`);
 }
